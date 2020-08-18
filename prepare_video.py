aa1a01608b7906359b914491d686df89a19b351b
Pavel <yagunovpavel@yandex.ru>
2020-08-18 17:01:35 +0300

    added image preparation

--- prepare_video.py
+++ prepare_video.py
@@ -2,35 +2,41 @@
 from moviepy.editor import *
 
 
-video = VideoFileClip("input.mp4")
+class VideoProcessor():
+    def process(self, video_filepath):
+        video = VideoFileClip(video_filepath)
 
-height = video.h
-width = video.w
+        height = video.h
+        width = video.w
 
-if height > width:
-    video = video.crop(x_center=width / 2, y_center=height / 2,
-                       width=width, height=width)
-else:
-    video = video.crop(x_center=width / 2, y_center=height / 2,
-                       width=height, height=height)
+        if height > width:
+            video = video.crop(x_center=width / 2, y_center=height / 2,
+                               width=width, height=width)
+        else:
+            video = video.crop(x_center=width / 2, y_center=height / 2,
+                               width=height, height=height)
 
-video = video.resize((256, 256))
+        video = video.resize((256, 256))
 
-iterator = 1
-duration = video.duration
-start_time = 0
-while start_time < duration:
-    if duration - start_time < 29:
-        output_clip = video.subclip(t_start=start_time, t_end=duration)
-        output_clip.write_videofile("output_video_" + str(iterator) + ".mp4", fps=25)
-        audioclip = output_clip.audio
-        audioclip.write_audiofile("output_audio_" + str(iterator) + ".mp3")
-        break
-    else:
-        output_clip = video.subclip(t_start=start_time, t_end=start_time + 29)
-        output_clip.write_videofile("output_video_" + str(iterator) + ".mp4", fps=25)
-        audioclip = output_clip.audio
-        audioclip.write_audiofile("output_audio_" + str(iterator) + ".mp3")
-        iterator += 1
-        start_time += 29
+        iterator = 1
+        duration = video.duration
+        start_time = 0
+        while start_time < duration:
+            if duration - start_time < 29:
+                output_clip = video.subclip(t_start=start_time, t_end=duration)
+                output_clip.write_videofile("output_video_" + str(iterator) + ".mp4", fps=25)
+                audioclip = output_clip.audio
+                audioclip.write_audiofile("output_audio_" + str(iterator) + ".mp3")
+                break
+            else:
+                output_clip = video.subclip(t_start=start_time, t_end=start_time + 29)
+                output_clip.write_videofile("output_video_" + str(iterator) + ".mp4", fps=25)
+                audioclip = output_clip.audio
+                audioclip.write_audiofile("output_audio_" + str(iterator) + ".mp3")
+                iterator += 1
+                start_time += 29
 
+if __name__ == '__main__':
+    video_filepath = 'input.mp4'
+    videoProcessor = VideoProcessor()
+    videoProcessor.process(video_filepath)
